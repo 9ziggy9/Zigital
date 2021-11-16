610588d719f7e495f3bafdd72af96b53b71aef06
Ziggy <davidarogers@protonmail.com>
2021-11-16 12:21:18 -0800

    added canvas proof of concept

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {useRef,
+               useEffect} from 'react';
 import "../../index.css";
 
 const Home = () => {
+  const canvasRef = useRef(null);
+
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    drawBackground(ctx);
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI);
+    ctx.fill();
+  }
+
+  const drawBackground = (ctx) => {
+    ctx.fillStyle = '#5fafd7';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    let frameCount = 0;
+    let animationFrameId;
+
+    const render = () => {
+      frameCount++;
+      draw(context, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    }
+
+    render();
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    }
+  }, [draw])
+
   return (
     <div className="canvas-area">
+      <canvas ref={canvasRef} />
     </div>
   )
 }
