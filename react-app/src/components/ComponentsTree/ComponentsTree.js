8f196ec73f0cd2bb3516e75d5fbb0261a8cce36e
Ziggy <davidarogers@protonmail.com>
2021-11-16 16:15:24 -0800

    switch prototype added

--- react-app/src/components/ComponentsTree/ComponentsTree.js
+++ react-app/src/components/ComponentsTree/ComponentsTree.js
@@ -2,6 +2,32 @@ import React from 'react';
 import "./ComponentsTree.css"
 
 const ComponentsTree = () => {
+
+  const selectGate = (id) => {
+    switch(id) {
+      case 'not':
+        console.log('GATE: not selected');
+        break;
+      case 'or':
+        console.log('GATE: or selected');
+        break;
+      case 'and':
+        console.log('GATE: and selected');
+        break;
+      case 'xor':
+        console.log('GATE: xor selected');
+        break;
+      case 'nor':
+        console.log('GATE: nor selected');
+        break;
+      case 'nand':
+        console.log('GATE: nand selected');
+        break;
+      case 'xnor':
+        console.log('GATE: xnor selected');
+        break;
+    }
+  }
   // const toggleMenu = (id, menu) => {
   //   const menuNode = document.getElementById(menu);
   //   const buttonNode = document.getElementById(id);
@@ -19,31 +45,31 @@ const ComponentsTree = () => {
   return (
     <>
       <div className='btn-list'>
-        <button id="not-btn">
+        <button id="not" onClick={() => selectGate('not')}>
           <img src="logic_gates/NOT.svg" alt=""/>
           <p>NOT</p>
         </button>
-        <button id="or-btn">
+        <button id="or" onClick={() => selectGate('or')}>
           <img src="logic_gates/OR.svg" alt=""/>
           <p>OR</p>
         </button>
-        <button id="and-btn">
+        <button id="and" onClick={() => selectGate('and')}>
           <img src="logic_gates/AND.svg" alt=""/>
           <p>AND</p>
         </button>
-        <button id="xor-btn">
+        <button id="xor" onClick={() => selectGate('xor')}>
           <img src="logic_gates/XOR.svg" alt=""/>
           <p>XOR</p>
         </button>
-        <button id="nor-btn">
+        <button id="nor" onClick={() => selectGate('nor')}>
           <img src="logic_gates/NOR.svg" alt=""/>
           <p>NOR</p>
         </button>
-        <button id="nand-btn">
+        <button id="nand" onClick={() => selectGate('nand')}>
           <img src="logic_gates/NAND.svg" alt=""/>
           <p>NAND</p>
         </button>
-        <button id="xnor-btn">
+        <button id="xnor" onClick={() => selectGate('xnor')}>
           <img src="logic_gates/XNOR.svg" alt=""/>
           <p>XNOR</p>
         </button>
