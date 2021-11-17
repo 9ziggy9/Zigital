e0f0ec23350aa9d42f9b3b3fa9dfcf7e9d308974
Ziggy <davidarogers@protonmail.com>
2021-11-16 17:11:12 -0800

    crease generic cell class

--- react-app/src/logic/classes/generic.js
+++ react-app/src/logic/classes/generic.js
@@ -5,7 +5,23 @@ export class Cell {
     this.width = cellSize;
     this.height = cellSize;
   }
+
   draw(ctx){
     ctx.strokeStyle = 'black';
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+export function createGrid(canvas, cellSize, grid) {
+  for (let y = cellSize; y < canvas.height; y += cellSize) {
+    for (let x = 0; x < canvas.width; x += cellSize) {
+      grid.push(new Cell(x, y, cellSize));
+    }
+  }
+}
+
+export function handleGrid(grid) {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].draw();
   }
 }
