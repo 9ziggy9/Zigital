a718120f7224ffb2a6a3fea1fdb076f12ce403db
Ziggy <davidarogers@protonmail.com>
2021-11-17 20:58:48 -0800

    good night

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -79,8 +79,11 @@ const Home = ({tool}) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     ctx.fillStyle = '#000000'
     ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+    ctx.arc(580, 360, 80*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
     ctx.fill()
+    if(!isDrawing) {
+      return
+    }
   }
 
   useEffect(() => {
