786abb1be71c5a2d1d63940161794f40a4d75f82
Ziggy <davidarogers@protonmail.com>
2021-11-17 19:59:57 -0800

    progress

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -75,14 +75,13 @@ const Home = ({tool}) => {
     handleGrid(GRID);
   }, []);
 
-   const draw = (ctx, frameCount) => {
+  const draw = (ctx, frameCount) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     ctx.fillStyle = '#000000'
     ctx.beginPath()
     ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
     ctx.fill()
   }
-
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -119,6 +118,8 @@ const Home = ({tool}) => {
       </div>
       <div className="canvas-area">
         <canvas
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
           ref={canvasRef}
         />
       </div>
