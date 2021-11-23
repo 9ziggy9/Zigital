9afd86e05c5f9dc65e516a2e3052fab35283b29a
Ziggy <davidarogers@protonmail.com>
2021-11-23 02:39:35 -0800

    css, daunting

---
+++ react-app/src/components/Splash/Splash.js
@@ -0,0 +1,29 @@
+import "./Splash.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import LogoutButton from '../auth/LogoutButton';
+
+const Splash = () => {
+  return (
+    <div className='navbar'>
+      <button id='home-btn' className='home'>
+          <NavLink to='/home' exact={true} activeClassName='active'>
+            Zigital
+          </NavLink>
+      </button>
+      <button id='login-btn'>
+          <NavLink to='/login' exact={true} activeClassName='active'>
+            Login
+          </NavLink>
+      </button>
+      <button id='signup-btn'>
+          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+            Sign Up
+          </NavLink>
+      </button>
+          {/* <LogoutButton /> */}
+    </div>
+  );
+}
+
+export default Splash;
