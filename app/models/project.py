2b5251a31216a7bdd7fe9bff2587fd935e1f80cf
Ziggy <davidarogers@protonmail.com>
2021-11-23 06:08:38 -0800

    db migrate relatively painlessly

--- app/models/project.py
+++ app/models/project.py
@@ -1,11 +1,14 @@
 from .db import db
 from sqlalchemy_json import mutable_json_type
+from sqlalchemy.dialects.postgresql import JSONB
 
 class Project(db.Model):
     __tablename__ = 'projects'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     title = db.Column(db.String(50))
     description = db.Column(db.String(500))
     state = db.Column(mutable_json_type(dbtype=JSONB, nested=True))
+
+    user = db.relationship("User", back_populates="project");
