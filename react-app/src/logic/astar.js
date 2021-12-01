52dd871089ff58cef3c0ae078c37785d60e57f32
Ziggy <davidarogers@protonmail.com>
2021-12-01 10:57:58 -0800

    modified heuristic

--- react-app/src/logic/astar.js
+++ react-app/src/logic/astar.js
@@ -2,39 +2,65 @@
 // REFERENCE: https://en.wikipedia.org/wiki/Taxicab_geometry#Formal_definition
 const taxiD = (a, b) => Math.abs(b.x - a.x) + Math.abs(b.y - a.y);
 
-function neighbors(cell, access, target = {x: 0, y: 0}) {
+// I add an additional element to the heuristic, we want to reduce number of turns
+// so I associate a cost if moving to a prospective neighbor is associated with a
+// turn. Since a and b are vectors from the origin and all steps have distance 1,
+// the difference b - a is equal to a +/- the unit vector in the x or y direction.
+// i.e. we're interested in the vector U = [b.x - a.x, b.y - a.y].
+// But we also need to know the previous unit vector associated with moving to point a
+// from a visited point, v, U0 = [a.x - v.x, a.y - v.y].
+// The magnitude of the cross product of these two vectors |U x U0| will tell us
+// if we are continuing along a path; if it is zero, U and U0 are parallel and we should
+// associate a lower cost.
+function turnCost(a,b,v) {
+    const U0 = {x: a.x - v.x, y: a.y - v.y};
+    const U = {x: b.x - a.x, y: b.y - a.y};
+    // Compute cross-product
+    const X = U0.x*U.y - U0.y*U.x;
+    return X === 0 ? 0 : 10;
+}
+
+function neighbors(cell, access, target = {x:0, y:0}, v={x:0,y:0}) {
     const {x, y} = cell.point;
     const neighbors = [];
     if (x + 1 < access[0].length && access[y][x+1]) {
         access[y][x+1] = 0;
+        const a = {x:x, y:y};
+        const b = {x: x+1, y:y};
         neighbors.push({
-            point: {x: x + 1, y: y},
-            parent: {x: x, y: y},
-            distance: taxiD({x: x + 1, y: y}, target)
+            point: b,
+            parent: a,
+            distance: taxiD(b, target) + turnCost(a,b,v)
         });
     }
     if (x - 1 >= 0 && access[y][x-1]) {
         access[y][x-1] = 0;
+        const a = {x:x, y:y};
+        const b = {x:x-1, y:y};
         neighbors.push({
-            point: {x: x - 1,y: y},
-            parent: {x: x,y: y},
-            distance: taxiD({x: x - 1,y: y}, target)
+            point: b,
+            parent: a,
+            distance: taxiD(b, target) + turnCost(a,b,v)
         });
     }
     if (y + 1 < access.length && access[y+1][x]) {
         access[y+1][x] = 0;
+        const a = {x:x, y:y};
+        const b = {x:x, y:y+1};
         neighbors.push({
-            point: {x: x,y: y + 1},
-            parent: {x: x,y: y},
-            distance: taxiD({x: x, y: y + 1}, target)
+            point: b,
+            parent: a,
+            distance: taxiD(b, target) + turnCost(a,b,v)
         });
     }
     if (y - 1 >= 0 && access[y-1][x]) {
         access[y-1][x] = 0;
+        const a = {x:x, y:y};
+        const b = {x:x, y:y-1};
         neighbors.push({
-            point: {x: x, y: y - 1},
-            parent: {x: x,y: y},
-            distance: taxiD({x: x, y: y - 1 }, target)
+            point: b,
+            parent: a,
+            distance: taxiD(b, target) + turnCost(a,b,v)
         });
     }
     return neighbors;
@@ -76,7 +102,12 @@ export function aStar(occupied, start, end) {
       const path = reconstructPath(visited);
       return path;
     }
-    queue.push(...neighbors(current, access, {x: end.point.x, y: end.point.y}));
+    if (visited.length > 1)
+        queue.push(...neighbors(current, access,
+                                {x: end.point.x, y: end.point.y},
+                                visited[visited.length - 1].parent));
+    else
+        queue.push(...neighbors(current, access, {x: end.point.x, y: end.point.y}));
     queue.shift();
     queue.sort((cell1, cell2) => cell1.distance - cell2.distance);
   }
